a9de7feccf6fa3b2bcfc7791f14ec64f9b6b5f74
RajPatel0111 <rajpatel031798@gmail.com>
2021-06-10 11:13:08 -0400

    Updated Footer Component

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -8,20 +8,20 @@ function Footer() {
                     <div className="col-7 col-md-8">
                         <h5>Dhrumil Shah</h5>
                         <i className="fa fa-phone fa-lg"></i>: +519-991-4992<br />
-                        <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:sdhrumil2605@gmail.com" >
-                            sdhrumil2605@gmail.com</a>
+                        <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:Torajpatel@outlook.com" >
+                            Torajpatel@outlook.com</a>
                     </div>
                     <div className="col-5 col-md-4 align-self-center">
                         <div className="text-center">
-                            <a className="btn btn-social-icon btn-github" href="https://github.com/DhrumilShah26/Tic-Tac-Toe" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
-                            <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/dhrumilshah26/" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
-                            <a className="btn btn-social-icon" href="mailto:sdhrumil2605@gmail.com"><i className="fa fa-envelope-o"></i></a>
+                            <a className="btn btn-social-icon btn-github" href="https://github.com/RajPatel0111/Tic-Tac-Toe-Game" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
+                            <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/rajpatel0111" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
+                            <a className="btn btn-social-icon" href="mailto:Torajpatel@outlook.com"><i className="fa fa-envelope-o"></i></a>
                         </div>
                     </div>
                 </div>
                 <div className="row justify-content-center">             
                     <div className="col-auto col-md-auto">
-                        <p>© Copyright 2020 Tic-Tac-Toe Inc.</p>
+                        <p>© Copyright 2021 Tic-Tac-Toe Inc.</p>
                     </div>
                 </div>
             </div>
