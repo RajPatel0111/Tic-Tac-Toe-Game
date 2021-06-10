4c51b5ae4857d391c0307440684ab90d3050b8c6
RajPatel0111 <rajpatel031798@gmail.com>
2021-06-10 11:19:28 -0400

    updated footer component

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -6,7 +6,7 @@ function Footer() {
             <div className="container">
                 <div className="row justify-content-center">             
                     <div className="col-7 col-md-8">
-                        <h5>Dhrumil Shah</h5>
+                        <h5>Raj Patel</h5>
                         <i className="fa fa-phone fa-lg"></i>: +226-507-7571<br />
                         <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:Torajpatel@outlook.com" >
                             Torajpatel@outlook.com</a>
